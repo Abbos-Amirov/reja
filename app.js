b479a7b909a8848a3c7958d369361d67f3c170e6
Abbos-Amirov <abbosamirov571@gmail.com>
2025-06-08 15:03:03 +0900

    CRUD darsi

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
-const express = require("express");
 
-module.exports = function (db) {
+  // return app;
+
+  const express = require('express');
   const app = express();
-
-  app.use(express.static("public"));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  // 2 Session code
-// 3 Vives code
-  app.set("views", "views");
-  app.set("view engine", "ejs");
-
-  app.get("/", function (req, res) {
-    console.log("user entered /")
-    db.collection("plans").find().toArray((err, data) => {
-      if (err) {
-        console.log(err);
-        return res.end("Xatolik yuz berdi");
+  const fs = require('fs');
+  const client = require('./server');
+  
+  let user;
+  fs.readFile("database/user.json", "utf-8", (err, data) => {
+      if(err){
+          console.log('ERROR:', err);
+      } else {
+          user = JSON.parse(data);
       }
-      res.render("reja", { plans: data }); // plans ni ejs ga uzatamiz
-    });
   });
-// 4 Routing code
-  app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    console.log("user entered / create-item")
-    db.collection("plans").insertOne({ text:req.body.reja })
-      .then(() => {
-        res.json({ message: "Qoshildi!" });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status().json({ error: "Xatolik!" });
+  
+  // MongoDB connect
+  const db = require('./server').db('reja');
+  
+  // 1: Kirish code
+  app.use(express.static('public'));
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+  
+  
+  // 2: Session code
+  
+  // 3: Views code
+  app.set('views', 'views');
+  app.set('view engine', 'ejs');
+  
+  // 4: Routing code
+  app.post('/create-item', (req, res) => {
+      console.log(req.body);
+      const new_reja = req.body.reja;
+     
+      db.collection('plans').insertOne({reja: new_reja}, (err, data) =>{
+          if(err) {
+              console.log(err);
+              res.end('something went wrong');
+          } else {
+              res.redirect('/');
+          }
       });
   });
-
-  return app;
-}
-
+  
+  // render  resume
+  app.get('/author', function (req, res) {
+      res.render('author', {user: user});
+  });
+  
+  // home page
+  app.get('/', function(req, res){
+      db.collection('plans')
+          .find()
+          .toArray((err, data) => {
+              if(err){
+                  console.log(err);
+                  res.end("something wnet wrong");
+              } else {
+                  console.log(data);
+                  res.render('reja', {items: data});
+              }
+      });
+  });
+  
+  module.exports = app;
 
 
 
--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 
 
-const http = require("http");
-const mongodb = require("mongodb");
+const http = require('http');
+const mongodb = require('mongodb')
 
-const connectionString = "mongodb+srv://Oscar:A3mzJqupDes8QLkx@cluster0.dbpygr2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const connectionString = "mongodb+srv://Oscar:A3mzJqupDes8QLkx@cluster0.dbpygr2.mongodb.net/reja"
 
-let db;
-
-mongodb.MongoClient.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
 }, (err, client) => {
-  if (err) {
-    console.log("MongoDB ulanishda xato:", err);
-  } else {
-    db = client.db("reja"); // Bazani tanladik
-    console.log("MongoDB connection succeed");
-
-    const app = require("./app")(db); // App'ga db ni uzatamiz
-    const server = http.createServer(app);
-    const PORT = 3000;
-    server.listen(PORT, () => {
-      console.log( `Server ishlayapti: http://localhost:${PORT}`);
-    });
-  }
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed")
+        module.exports = client;
+        const app = require('./app');
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function(){
+            console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`); 
+});
+    }
 });
 
 
